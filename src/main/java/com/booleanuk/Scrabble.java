4875d8cc840ca12504ad902597674e581b8923ff
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-13 17:48:41 +0200

    Finished extension

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -8,11 +8,10 @@ import java.util.Stack;
 public class Scrabble {
 public String word;
 public HashMap<Character, Integer> points = new HashMap<>();
-
+public int totalScore = 0;
 
     public Scrabble(String word) {
         this.word = word.toLowerCase();
-
         this.points.put('a', 1);
         this.points.put('b', 3);
         this.points.put('c', 3);
@@ -42,45 +41,152 @@ public HashMap<Character, Integer> points = new HashMap<>();
     }
 
 
+    public int score() {
+
+        if (!isValid() | word.isEmpty()) {
+            return 0;
+        }
+
+        doubleLetters();
+        tripleLetters();
+
+        for (int i = 0; i < this.word.length(); i++){
+            if (points.containsKey(this.word.charAt(i))) {
+                // If the previous index does not contain \
+                if (i == 0 || this.word.charAt(i-1) != '\\') {
+                    totalScore += points.get(this.word.charAt(i));
+                }
+            }
+        }
+
+        while(this.word.charAt(0) == '{' || this.word.charAt(0) == '[')
+        {
+            if (this.word.charAt(0) == '{') {
+                doubleWord();
+            }
+            else if (this.word.charAt(0) == '['){
+                tripleWord();
+            }
+        }
+
+        return totalScore;
+    }
+
+
     public boolean isValid() {
         Stack<Character> stack = new Stack<>();
+        int letterCounter = 0;
+
         for (int i = 0; i < this.word.length(); i++) {
-            if (Objects.equals(this.word.charAt(i), '{') | Objects.equals(this.word.charAt(i), '[')) {
+
+            if (this.word.charAt(i) == '{') {
+                if (letterCounter != 0 & this.word.charAt(i+2) != '}'){
+                    return false;
+                }
                 stack.push(this.word.charAt(i));
             }
 
-            else if (Objects.equals(this.word.charAt(i), '}')) {
-                if (Objects.equals(stack.peek(), '{')){
+            else if (this.word.charAt(i) == '[') {
+                if (letterCounter != 0 & this.word.charAt(i+2) != ']'){
+                    return false;
+                }
+
+                stack.push(this.word.charAt(i));
+            }
+
+            else if (this.word.charAt(i) == '}') {
+
+                if (stack.empty()){
+                    return false;
+                }
+
+                else if (stack.peek() == '{'){
                     stack.pop();
                 }
             }
 
-            else if (Objects.equals(this.word.charAt(i), ']')) {
-                if (Objects.equals(stack.peek(), '[')){
+            else if (this.word.charAt(i) == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (stack.peek() == '[') {
                     stack.pop();
                 }
+            }
+
+            else if (this.word.charAt(i) == '!' || this.word.charAt(i) ==
+                    '|' || this.word.charAt(i) == ' ') {
+                return false;
+            }
+
+            else {
+                letterCounter++;
             }
         }
         return stack.isEmpty();
     }
 
-    public int score() {
-        int score = 0;
 
-
-        for (int i = 0; i < this.word.length(); i++){
-
-            if(this.word.charAt(i) == ' ') {
-                score += 0;
+    public void doubleLetters(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++){
+            if (Objects.equals(this.word.charAt(i), '{')){
+                if (this.word.charAt(i+2) == '}'){
+                    totalScore += points.get(word.charAt(i+1)) * 2;
+                    i+=2;
+                }
+                else {
+                    sb.append(word.charAt(i));}
             }
 
-            else if (points.containsKey(this.word.charAt(i))) {
-                if (i == 0 || this.word.charAt(i-1) != '\\') {
-                    score += points.get(this.word.charAt(i));
-                }
+            else {
+                sb.append(word.charAt(i));
             }
         }
+        this.word = sb.toString();
+    }
 
-        return score;
+    public void tripleLetters(){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++){
+            if (Objects.equals(this.word.charAt(i), '[')){
+                if (this.word.charAt(i+2) == ']'){
+                    totalScore += points.get(word.charAt(i+1)) * 3;
+                    i+=2;
+                }
+                else {
+                    sb.append(word.charAt(i));
+                }
+            }
+
+            else {
+                sb.append(word.charAt(i));
+            }
+        }
+        this.word = sb.toString();
+    }
+
+    public void doubleWord(){
+
+        StringBuilder sb = new StringBuilder();
+        if (this.word.charAt(0) == '{' & this.word.charAt(this.word.length() - 1) == '}') {
+            sb.append(this.word);
+            sb.deleteCharAt(this.word.length()-1);
+            sb.deleteCharAt(0);
+        }
+        this.word = sb.toString();
+        this.totalScore = totalScore*2;
+    }
+
+    public void tripleWord(){
+        StringBuilder sb = new StringBuilder();
+        if (this.word.charAt(0) == '[' & this.word.charAt(this.word.length() - 1) == ']') {
+            sb.append(this.word);
+            sb.deleteCharAt(this.word.length()-1);
+            sb.deleteCharAt(0);
+        }
+        this.word = sb.toString();
+        this.totalScore = this.totalScore * 3;
     }
 }
+
